79f1146f8100476f7c24d081b84c3f6b2c26f78c
coto24 <jdelpino2020@alu.uct.cl>
2024-11-24 20:37:10 -0300

    DESPLIEGUE INTENTO

--- agenda-web/src/index.jsx
+++ agenda-web/src/index.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App';
 
+const port = process.env.PORT || 3000;
+App.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
